304b72874c063933528dbf4e3277661015231161
Nicholas Parise <1treasurehound@gmail.com>
2025-01-30 17:58:09 -0500

    adding auth and other API endpoints

--- backend/events.js
+++ backend/events.js
@@ -2,28 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 let eventEntry = [
-  {id:0, name:'birthday', owner:'admin', description:'video game console', date:'Jan 16 2025', picture:'assets/placeholder-events.png'},
-  {id:1, name:'birthday again', owner:'bdmin', description:'video game console', date:'Jan 16 2025', picture:'assets/placeholder-events.png'}
+  {id:0, user_id:0, name:'birthday', description:'video game console', url:'', addr:'', city:'', image:'assets/placeholder-events.png', datecreated:'2025-1-29', dateupdated:'2025-1-29'},
+  {id:1, user_id:0, name:'birthday again', description:'video game console', url:'', addr:'', city:'', image:'assets/placeholder-events.png', datecreated:'2025-1-29', dateupdated:'2025-1-29'}
 ];
 
 
-// localhost:3000/events?page=1&pageSize=10
+// localhost:3000/events?page=1&pageSize=10&member=admin
+// get all the events a user is in
 router.get('/',(req,res,next)=>{
     
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.pageSize) || 10;
-    
+    const member = parseArgs(req.query.member) || 0; // idk if this is the right one I haven't run this yet, it should just expect a string
+
     const eventItems = eventEntry.slice((page - 1) * pageSize, page * pageSize);
 
     res.json({
         page,
         pageSize,
-        totalItems: eventEntry.length,
-        items: eventItems,
+        totalEvents: eventEntry.length,
+        events: eventItems,
       });
 })
 
 // localhost:3000/events/0
+// get event details
 router.get('/:eventId', (req, res, next) => {
     const eventId = parseInt(req.params.eventId);
   
@@ -35,14 +38,62 @@ router.get('/:eventId', (req, res, next) => {
   });
 
 
-  router.post('/add', (req, res) => {
+// localhost:3000/events/0
+// update event details
+router.put('/:eventId', (req, res, next) => {
+  const eventId = parseInt(req.params.eventId);
 
-    eventEntry.push({id: req.body.id, 
+  eventEntry.forEach(event => {
+    if (event.id === eventId){
+      event.owner = req.body.user_id,
+      event.name = req.body.name,
+      event.description = req.body.description,
+      event.url = req.body.url,
+      event.addr = req.body.addr,
+      event.city = req.body.city,
+      event.image = req.body.image,
+      event.date = req.body.date 
+    }
+  });
+
+  res.status(200).json({
+      message: 'Post submitted'
+  });
+});
+
+
+// localhost:3000/events/0
+// delete event details
+router.delete('/:eventId', (req, res, next) => {
+  const eventId = parseInt(req.params.eventId);
+
+  eventEntry.forEach(event => {
+    if (event.id === eventId){
+        
+    }
+  });
+
+  res.status(200).json({
+      message: 'Post submitted'
+  });
+});
+
+
+
+  // create a new event
+  router.post('/', (req, res) => {
+
+    eventEntry.push({
+      id: req.body.id, 
+      owner: req.body.user_id,
       name: req.body.name,
-      owner: req.body.owner,
       description: req.body.description,
-      date: req.body.date, 
-      picture: req.body.picture});
+      url: req.body.url,
+      addr: req.body.addr,
+      city: req.body.city,
+      image: req.body.image,
+      date: req.body.date 
+    });
     
     res.status(200).json({
         message: 'Post submitted'
--- backend/items.js
+++ backend/items.js
@@ -3,17 +3,17 @@ const { parseArgs } = require('util');
 const router = express.Router();
 
 let itemEntry = [
-  {id:0, name:'PS5', description:'video game console', date:'Jan 16 2025', picture:'assets/placeholder-item.png'},
-  {id:1, name:'Switch 2', description:'video game console', date:'Jan 16 2025', picture:'assets/placeholder-item.png'}
+  {id:0, member_id:0, wishlist_id:0, name:'PS5', description:'video game console', url:'', image:'public/placeholder-item.png', quantity:1, price:10.0, datecreated:'2025-1-29', dateupdated:'2025-1-29'},
+  {id:1, member_id:0, wishlist_id:0, name:'Switch 2', description:'video game console', url:'', image:'public/placeholder-item.png', quantity:1, price:10.0, datecreated:'2025-1-29', dateupdated:'2025-1-29'}
 ];
 
-
-// localhost:3000/items?page=1&pageSize=10&event=somevalue
+// localhost:3000/items?page=1&pageSize=10&wishlist=somevalue
+// get all items from a wishlist
 router.get('/',(req,res,next)=>{
     
     const page = parseInt(req.query.page) || 1;
     const pageSize = parseInt(req.query.pageSize) || 10;
-    const event = parseArgs(req.query.event) || "somevalue"; // idk if this is the right one I haven't run this yet, it should just expect a string
+    const wishlist = parseArgs(req.query.wishlist) || 0; // idk if this is the right one I haven't run this yet, it should just expect a string
     
     const paginatedItems = itemEntry.slice((page - 1) * pageSize, page * pageSize);
 
@@ -26,6 +26,7 @@ router.get('/',(req,res,next)=>{
 })
 
 // localhost:3000/items/0
+// get the contents of a single item
 router.get('/:itemId', (req, res, next) => {
     const itemsId = parseInt(req.params.itemId);
   
@@ -36,14 +37,19 @@ router.get('/:itemId', (req, res, next) => {
     });
   });
 
-
+  
   router.post('/add', (req, res) => {
 
-    itemEntry.push({id: req.body.id, 
+    itemEntry.push({
+      id: req.body.id, 
       name: req.body.name,
       description: req.body.description,
-      date: req.body.date, 
-      picture: req.body.picture});
+      url: req.body.url,
+      image: req.body.image,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      date: req.body.date 
+    });
     
     res.status(200).json({
         message: 'Post submitted'
