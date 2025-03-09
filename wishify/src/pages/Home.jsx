8c035ac1f6f645788dc62ed0eef33e33bcb93b93
Ethan Brennan <125213920+Thunderraker@users.noreply.github.com>
2025-03-09 16:00:43 -0400

    Home Page Pic Placeholder
    Added placeholder for user profile picture and username on the Home page.

--- wishify/src/pages/Home.jsx
+++ wishify/src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
-import Navbar from '../components/Navbarmain'
+import Navbar from '../components/Navbarmain.jsx'
+import '../home.css'
 
 const Home = () => {
+  const [user, setUser] = React.useState({
+      profilePictureURL: "",
+      displayName: "John Doe",
+      email: "johndoe@wishify.com",
+    })
+
   return (
     <>
       <Navbar></Navbar>
-      <div>Home</div>
+      <br/>
+      <section className="home-container">
+        <div className="home-user-info">
+
+          <div className="home-user-picture">
+            <img
+              src={user.profilePicture || "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?t=st=1738333167~exp=1738336767~hmac=d1a2645bf22eff4e35bc060e5a7529cb9cbf09696ae232ab6690c137ad06d5e4&w=1060"}
+              alt='User profile picture'
+            />
+          </div>
+
+          <div className="home-welcome-text">
+            <h2>Welcome back, {user.displayName}!</h2>
+          </div>
+        </div>
+
+      </section>
     </>
   )
 }
