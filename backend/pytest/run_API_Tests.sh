fc6b213d351b3aa789684d37f3db4ba0c40361e9
Geoffrey Jensen <93730621+Gexff@users.noreply.github.com>
2025-03-14 10:46:06 -0400

    Switching to absolute paths

--- backend/pytest/run_API_Tests.sh
+++ backend/pytest/run_API_Tests.sh
@@ -7,17 +7,17 @@ MAX_HISTORY_SIZE=48
 
 cd /var/www/4P02-course-project/backend/pytest
 
-pytest /var/www/4P02-course-project/backend/pytest --json-report --json-report-omit keywords streams root --json-report-verbosity 2 --json-report-indent 2 --json-report-file "$REPORT_DIR""$REPORT_FILE"
+/usr/bin/pytest --json-report --json-report-omit keywords streams root --json-report-verbosity 2 --json-report-indent 2 --json-report-file "$REPORT_DIR""$REPORT_FILE"
 
-python3 process_test_report.py "$REPORT_DIR" "$REPORT_FILE" "$MAX_HISTORY_SIZE" 
+/usr/bin/python3 process_test_report.py "$REPORT_DIR" "$REPORT_FILE" "$MAX_HISTORY_SIZE" 
 
-ln -sfT "${REPORT_DIR}${PROCESSED_REPORT}_0" api_status_report
+/usr/bin/ln -sfT "${REPORT_DIR}${PROCESSED_REPORT}_0" api_status_report
 
 for ((i="$MAX_HISTORY_SIZE"; i>=1; i--))
 do
     if [ -f "${REPORT_DIR}${PROCESSED_REPORT}_${i}" ]; then
         END=$((i+1))
-        mv "${REPORT_DIR}${PROCESSED_REPORT}_${i}" "${REPORT_DIR}${PROCESSED_REPORT}_${END}"
+        /usr/bin/mv "${REPORT_DIR}${PROCESSED_REPORT}_${i}" "${REPORT_DIR}${PROCESSED_REPORT}_${END}"
     fi
 done
 
@@ -28,6 +28,6 @@ if [ -f "${REPORT_DIR}${PROCESSED_REPORT}_${END}" ]; then
 fi
 
 
-cp "${REPORT_DIR}${PROCESSED_REPORT}_0" "${REPORT_DIR}${PROCESSED_REPORT}_1"
+/usr/bin/cp "${REPORT_DIR}${PROCESSED_REPORT}_0" "${REPORT_DIR}${PROCESSED_REPORT}_1"
 
-ln -sfT "${REPORT_DIR}${PROCESSED_REPORT}_1" api_status_report
+/usr/bin/ln -sfT "${REPORT_DIR}${PROCESSED_REPORT}_1" api_status_report
