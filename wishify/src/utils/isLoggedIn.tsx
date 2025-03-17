9cc84a79aaa59a137e6a94e3182290aa887ba9cc
Geoffrey Jensen <geoffrey.jensen@cogeco.ca>
2025-03-17 19:15:13 -0400

    adding check for expired token

--- wishify/src/utils/isLoggedIn.tsx
+++ wishify/src/utils/isLoggedIn.tsx
@@ -1,12 +1,31 @@
+import { jwtDecode } from "jwt-decode";
+
 export default function isLoggedIn(){
     const token: string = localStorage.getItem('token') || ""
 
-    if(token === ""){
-        return false
-    }
-
-    // check if token is expired here...
-
+    if(token === "") return false
+    if(hasTokenExpired(token)) return false
 
     return true
 }
+
+function hasTokenExpired(token: string) {
+    try {
+      // Decode the token to get the payload
+      const decoded = jwtDecode(token);
+
+      if (decoded.exp === undefined) return true
+  
+      const currentTime = Date.now() / 1000;
+  
+      // Check if the token is expired
+      if (decoded.exp < currentTime) {
+        return true; // Token has expired
+      } else {
+        return false; // Token has not expired
+      }
+    } catch (error) {
+      console.error('Invalid token', error);
+      return true; // Return true if token decoding fails (invalid token)
+    }
+  }
