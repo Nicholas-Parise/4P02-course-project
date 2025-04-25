52b494545be3b0c9a21b5943bcb254766b0bbbcf
Nicholas Parise <1treasurehound@gmail.com>
2025-04-25 12:17:10 -0400

    fixing 404 page

--- wishify/src/pages/NoPage.jsx
+++ wishify/src/pages/NoPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+import "./NotFound.css";
 
 const NoPage = () => {
-    return (
+  const navigate = useNavigate();
+
+
+  function goHome(){
+    navigate('/home');
+  }
+
+  return (
     <>
-      <h1>404</h1>
+      <div className="notfound-container">
+        <h1 className="notfound-status">404</h1>
+        <h2 className="notfound-title">Oops! Page not found.</h2>
+        <p className="notfound-text">
+          The page you're looking for doesn't exist or has been moved.
+        </p>
+        <button type="button" onClick={goHome} className="notfound btn">
+          Go back home
+        </button>
+      </div>
     </>
   );
-  };
-  
+};
+
 export default NoPage;
